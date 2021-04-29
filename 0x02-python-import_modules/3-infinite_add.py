87ff327a1a233b196c505b48f1de8fc20a440a34
MohamadDabboussi <dabboussi99@gmail.com>
2021-04-29 18:54:40 +0300

    add task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,9 +6,8 @@ def main(argv):
     Num_arg = len(argv)
     result = 0
     for i in range(Num_arg):
-         result += int(argv[i])
+        result += int(argv[i])
     print('{:d}'.format(result))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
