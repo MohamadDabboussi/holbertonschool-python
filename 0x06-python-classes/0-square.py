ba9503485823bf006efb8bd4ecc9e9b245c470ba
MohamadDabboussi <dabboussi99@gmail.com>
2021-05-13 23:36:32 +0300

    add task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""A module with an empty class"""
+"""
+    0-square.py
+    Module defining square
+    return {}
+"""
 
 
 class Square:
-    """An empty class called Square"""
+    """Square class
+       This will define a square and will return an empty block
+    """
     pass
