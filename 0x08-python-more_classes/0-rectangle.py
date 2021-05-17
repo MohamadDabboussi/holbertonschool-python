10464f60da792beae92a13b7df66493ccc583475
MohamadDabboussi <dabboussi99@gmail.com>
2021-05-18 00:07:15 +0300

    add task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Module defining rectangle
+    return {}
+"""
+
+
+class Rectangle:
+    """Rectangle class
+       Defines an empty rectangle and returns nothing
+    """
+    pass
